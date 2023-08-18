5206d035dc7bd4fe7051e5d493e58d410d4535ad
André Pennini <andrepennini@usp.br>
2023-08-18 12:14:59 -0300

    mail merge finished

--- projects/intermediate/mail-merge/main.py
+++ projects/intermediate/mail-merge/main.py
@@ -1,4 +1,4 @@
-#TODO: Create a letter using starting_letter.txt 
+# Create a letter using starting_letter.txt 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -7,3 +7,14 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+with open("./Input/Letters/starting_letter.txt", "r") as file_letter:
+    message = file_letter.read()
+
+with open("./Input/Names/invited_names.txt", "r") as file_names:
+        names = file_names.readlines()
+
+        for name in names:
+            name = name.strip()
+            new_letter = message.replace("[name]", name)
+            with open(f"./Output/ReadyToSend/letter_for_{name}.txt", "w") as file_send:
+                file_send.write(new_letter)
