26450b58f0d06bab069af6ea801ad7edfb90bf44
André Pennini <andrepennini@usp.br>
2023-08-21 11:25:01 -0300

    day 25 finished - improvements US state game

--- projects/intermediate/pandas/us-states/main.py
+++ projects/intermediate/pandas/us-states/main.py
@@ -22,25 +22,30 @@ tr.shape(image)
 # tr.mainloop()
 df_states = pd.read_csv("50_states.csv")
 lenght_game = len(df_states["state"])
-count = 0
+count = []
 is_game_on = True
 while is_game_on:
     screen.update()
-    answer_state = screen.textinput(title="Guess the state", prompt="What's the state name you want to try?")
-    answer = answer_state.title()
+    answer_state = screen.textinput(title="Guess the state", prompt="What's the state name you want to try?").title()
 
-    if answer in list(df_states["state"]):
-        count += 1
-        state = df_states[df_states["state"] == answer]
+    if answer_state == "Exit":
+        is_game_on = False
+        not_guessed = []
+        for state in df_states["state"]:
+            if state not in count:
+                not_guessed.append(state)
+        not_guessed = pd.DataFrame(not_guessed)
+        not_guessed.to_csv("states_not_guessed.csv")
+
+    if answer_state in list(df_states["state"]) and answer_state not in count:
+        count.append(answer_state)
+        state = df_states[df_states["state"] == answer_state]
         x_ans = int(state["x"])
         y_ans = int(state["y"])
         turtle.goto(x_ans, y_ans)
-        turtle.write(answer, align="center", font=["Arial", 8, "normal"])
+        turtle.write(answer_state)
 
-    if count == lenght_game:
+    if len(count) == lenght_game:
         turtle.goto(-25,200)
         turtle.write("Congratulations, you've won", align="center", font=["Arial", 30, "normal"])
         is_game_on = False
-
-
-screen.exitonclick()
