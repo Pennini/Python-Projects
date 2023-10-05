1bbd5666dcdbed274a80e93715aae33d364fa7b8
André Pennini <andrepennini@usp.br>
2023-10-05 19:32:50 -0300

    get stock api alpha vantage finished + start get news api

--- projects/intermediate+/api-authentication/main.py
+++ projects/intermediate+/api-authentication/main.py
@@ -1,1 +1,23 @@
+import requests
+
+url = "https://api.openweathermap.org/data/2.8/onecall"
+api_key = "0c86e4e6f6f41e87f7018eebe8b768dd"
+
+parameters = {
+    "lat": -23.550520,
+    "lon": -46.633308,
+    "exclude": "current,minutely,daily",
+    "appid": api_key
+}
+
+response = requests.get(url, params=parameters)
+response.raise_for_status()
+data = response.json()["hourly"]
+
+for hour in data[:12]:
+    if int(hour["weather"][0]["id"]) < 700:
+        print("It will rain", hour)
+        break
+
+
 
---
+++ projects/intermediate+/stock-news-extrahard/main.py
@@ -0,0 +1,74 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+import sys
+import requests
+import datetime as dt
+
+
+caminho_diretorio = r"C:\Users\Lenovo\Desktop\FEA DEV\fin_quant\curso"
+sys.path.append(caminho_diretorio)
+
+from secrets_api import ALPHA_VANTAGE_API_TOKEN, NEWS_API_KEY
+
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={STOCK}&interval=5min&apikey=demo"
+
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+def get_stock(stock: str):
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey={ALPHA_VANTAGE_API_TOKEN}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()["Time Series (Daily)"]
+
+    yesterday = str(dt.datetime.now() - dt.timedelta(days=1))[:10]
+    before_yesterday = str(dt.datetime.now() - dt.timedelta(days=2))[:10]
+
+    try:
+        yesterday_price = float(data[yesterday]["4. close"])
+        before_yesterday_price = float(data[before_yesterday]["4. close"])
+    except TypeError:
+        print("Must be a number")
+        return
+    except Exception as mes:
+        print(f"Error getting stock price\n{mes}")
+        return
+
+    percentage = round(
+        ((yesterday_price - before_yesterday_price) / before_yesterday_price) * 100, 2
+    )
+    if percentage < -5 or percentage > 5:
+        return True, percentage
+    else:
+        return False, percentage
+
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+def get_news(company):
+    url = f"https://newsapi.org/v2/everything?q={company}&from=2023-09-05&sortBy=publishedAt&apiKey=1fdf670c22a443a79b5e76922f3106ea"
+
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    print(data)
+
+
+## STEP 3: Use https://www.twilio.com
+# Send a separate message with the percentage change and each article's title and description to your phone number.
+
+
+# Optional: Format the SMS message like this:
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
+result, percentage = get_stock(STOCK)
+
+get_news(COMPANY_NAME)
